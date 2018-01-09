4fd94d3ed2628c4286fd3b67ccb300fd8c2525c4
Alexia Elise Nunez <alexianunez78@gmail.com>
2018-01-09 13:28:14 -0800

    initial commit of xcode project

---
+++ so-test-app/so-test-appTests/so_test_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  so_test_appTests.swift
+//  so-test-appTests
+//
+//  Created by Alexia Nunez on 1/9/18.
+//  Copyright © 2018 Alexia Nunez. All rights reserved.
+//
+
+import XCTest
+@testable import so_test_app
+
+class so_test_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
